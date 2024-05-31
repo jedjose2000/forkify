dc30dfa35bbdfdc84f774eea141cc77ac30d8554
jedjose2000 <jedjose2000@gmail.com>
2024-05-31 12:28:55 +0800

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const API_FORKIFY_KEY = '0f815d2d-4f63-4925-abc8-cb51b2ccc293';
+export const MODAL_CLOSE_SECONDS = 2.5;
